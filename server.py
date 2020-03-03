81a98b82248126884d8364ccb430805685c3ce58
itGabriel <gabriel.gustavo.s@hotmail.com>
2020-03-03 17:55:25 -0300

    Estruturação do for de dados

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, escape, render_template, request, redirect, session, flash, url_for
 from services import client as cliente
 from bd import puppy_bd as scriptPUPPYDATABASE
+import json
 
 app = Flask(__name__)
 
@@ -18,8 +19,8 @@ def clientes():
 @app.route('/clientes/all')
 def clientes_todos():
         items = cliente.select_all()
-        print(items)
-        return render_template('dados_cliente.html', lista_clientes = items)
+        dados = {'id': items[0][0], 'nome': items[0][2], 'cpf': items[0][1], 'endereco': items[0][3], 'telefone_residencial': items[0][4], 'telefone_celular': items[0][5]}
+        return render_template('dados_cliente.html', lista_clientes = dados)
     # try:
     #     # return cliente.select_all()
     # except:
@@ -42,12 +43,26 @@ def clientes_cadastro():
     except:
         return "Falha em realizar cadastro de clientes"
 
-### FIM OTAS CLIENTE !!!
+@app.route('/clientes/buscar-cliente')
+def buscar_cliente():
+    idCliente = request.args['idCliente']
+    try:
+        busca = cliente.busca_por_id(idCliente)
+        print(busca)
+        return busca
+    except:
+        return "Falha em realizar busca do client pelo id {}".format(idCliente)
+
+
+### FIM ROAS CLIENTE !!!
+
+
+
+
+
 
 
 scriptPUPPYDATABASE.criar_muitasTabelas()
-
-
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port='4811')
     
--- services/client.py
+++ services/client.py
@@ -16,6 +16,18 @@ def select_all():
 
 # ''''A SER FEITO''' 
 # SELECT DOS DADOS DA TABELA 'X' FILTRANDO POR ID OU 'TEXTO' COMPLETO INFORMADO PELO USUÁRIO ATRAVÉS(SEGUNDA OPÇÃO) DO CAMPO PRINCIPAL/ALTERNATIVO DO ID
+def busca_por_id(idCliente):
+    connection = sqlite3.connect('db_puppy.db')
+    cursor = connection.cursor()
+    sql = "SELECT * FROM Cliente WHERE id_cliente = {}".format(idCliente)
+    id_cliente = cursor.lastrowid
+    cursor.execute(sql, (idCliente))
+    cursor.close()
+    connection.commit()
+    connection.close()
+    return id_cliente
+
+
 
 
 # INSERT NA TABELA 'Cliente' UTILIZANDO 'AUTO_INCREMENT' 
@@ -30,3 +42,14 @@ def insert_dados(nome, cpf, endereco, telefone_residencial, telefone_celular):
     connection.close()
     return id_cliente
 
+
+def remover_por_id(idCliente):
+    connection = sqlite3.connect('db_puppy.db')
+    cursor = connection.cursor()
+    sql = "DELETE FROM Cliente WHERE id_cliente = {}".format(idCliente)
+    id_cliente = cursor.lastrowid
+    cursor.execute(sql, (idCliente))
+    cursor.close()
+    connection.commit()
+    connection.close()
+    return id_cliente
